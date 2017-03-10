385fb9330bb1cb5c0d3b156a0c517341e758488f
Tyler Johnson <mail@tyler-johnson.ca>
2017-03-10 11:32:51 -0500

    Fix build script call

--- include-deps.sh
+++ include-deps.sh
@@ -1,13 +1,13 @@
-mkdir -p dist/scripts/slider-revolutions/css
-curl -O "http://s3.amazonaws.com/rise-common/scripts/slider-revolution/css/style.css" dist/scripts/slider-revolution/css/style.css
-curl -O "http://s3.amazonaws.com/rise-common/scripts/slider-revolution/css/settings.css" dist/scripts/slider-revolution/css/settings.css
+mkdir -p dist/scripts/slider-revolution/css
+curl "http://s3.amazonaws.com/rise-common/scripts/slider-revolution/css/style.css" -o dist/scripts/slider-revolution/css/style.css
+curl "http://s3.amazonaws.com/rise-common/scripts/slider-revolution/css/settings.css" -o dist/scripts/slider-revolution/css/settings.css
 
-mkdir -p dist/scripts/slider-revolutions/js
-curl -O "http://s3.amazonaws.com/rise-common/scripts/slider-revolution/js/jquery.themepunch.tools.min.js" dist/scripts/slider-revolution/js/jquery.tools.min.js
-curl -O "http://s3.amazonaws.com/rise-common/scripts/slider-revolution/js/jquery.themepunch.revolution.min.js" dist/scripts/slider-revolution/js/jquery.themepunch.revolution.min.js
+mkdir -p dist/scripts/slider-revolution/js
+curl "http://s3.amazonaws.com/rise-common/scripts/slider-revolution/js/jquery.themepunch.tools.min.js" -o dist/scripts/slider-revolution/js/jquery.tools.min.js
+curl "http://s3.amazonaws.com/rise-common/scripts/slider-revolution/js/jquery.themepunch.revolution.min.js" -o dist/scripts/slider-revolution/js/jquery.themepunch.revolution.min.js
 
 mkdir -p dist/jquery
-curl -O "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" dist/jquery/jquery.min.js
+curl "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" -o dist/jquery/jquery.min.js
 
 mkdir -p dist/gadgets
-curl --compressed -O "http://rvashow2.appspot.com/gadgets/gadgets.min.js" dist/gadgets/gadgets.min.js
+curl --compressed "http://rvashow2.appspot.com/gadgets/gadgets.min.js" -o dist/gadgets/gadgets.min.js

a148ac325ed536454835a46c528d5068f70db8e5
Stuart Lees <stuartlees@gmail.com>
2018-03-29 18:49:58 -0400

    player local storage progress [stage-2]

--- src/widget/player-local-storage-file.js
+++ src/widget/player-local-storage-file.js
@@ -5,11 +5,67 @@ var RiseVision = RiseVision || {};
 
 RiseVision.Image = RiseVision.Image || {};
 
-RiseVision.Image.PlayerLocalStorageFile = function() {
+RiseVision.Image.PlayerLocalStorageFile = function( params ) {
   "use strict";
 
-  var wsClient = new localMessaging.default(),
-    storage = null; // eslint-disable-line no-unused-vars
+  var INITIAL_PROCESSING_DELAY = 10000,
+    messaging = new localMessaging.default(),
+    storage = null,
+    initialProcessingTimer = null,
+    watchInitiated = false,
+    initialLoad = true;
+
+  function _clearInitialProcessingTimer() {
+    clearTimeout( initialProcessingTimer );
+    initialProcessingTimer = null;
+  }
+
+  function _startInitialProcessingTimer() {
+    _clearInitialProcessingTimer();
+
+    initialProcessingTimer = setTimeout( function() {
+
+    }, INITIAL_PROCESSING_DELAY );
+  }
+
+  function _getStoragePath() {
+    var path = "";
+
+    if ( params.storage.folder ) {
+      path += params.storage.folder + ( params.storage.folder.slice( -1 ) !== "/" ? "/" : "" );
+    }
+
+    path += params.storage.fileName;
+
+    return "risemedialibrary-" + params.storage.companyId + "/" + path;
+  }
+
+  function _getSingleFileURL() {
+    return "https://storage.googleapis.com/" + encodeURIComponent( _getStoragePath() );
+  }
+
+  function _handleAuthorized() {
+    RiseVision.Image.logEvent( {
+      "event": "authorized"
+    } );
+
+    if ( !watchInitiated ) {
+      storage.watchFiles( _getSingleFileURL() );
+      watchInitiated = true;
+    }
+  }
+
+  function _handleFileProcessing() {
+    if ( initialLoad ) {
+      //RiseVision.Image.onFileUnavailable( "File is downloading" );
+      _startInitialProcessingTimer();
+    } else {
+      _clearInitialProcessingTimer();
+
+    }
+  }
+
+  function _handleFileAvailable() {}
 
   function _handleEvents( data ) {
     if ( !data || !data.event || typeof data.event !== "string" ) {
@@ -29,20 +85,36 @@ RiseVision.Image.PlayerLocalStorageFile = function() {
       } );
       break;
     case "AUTHORIZED":
-      RiseVision.Image.logEvent( {
-        "event": "authorized"
-      } );
+      _handleAuthorized();
       break;
     case "UNAUTHORIZED":
       RiseVision.Image.logEvent( {
         "event": "unauthorized"
       } );
       break;
+    case "FILE-AVAILABLE":
+      _handleFileAvailable( data );
+      break;
+    case "FILE-PROCESSING":
+      _handleFileProcessing();
+      break;
+    case "FILE-NO-EXIST":
+      //
+      break;
+    case "FILE-DELETED":
+      //
+      break;
+    case "FILE-ERROR":
+      //
+      break;
     }
   }
 
   function init() {
-    storage = new playerLocalStorage.default( wsClient, _handleEvents );
+
+
+
+    storage = new playerLocalStorage.default( messaging, _handleEvents );
   }
 
   return {

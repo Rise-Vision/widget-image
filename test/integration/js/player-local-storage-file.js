3ce85f3325aa81ebde75f20d674381795acdf097
Stuart Lees <stuartlees@gmail.com>
2018-04-05 17:13:34 -0400

    Add RLS single file and image-utils integration tests [stage-2]

---
+++ test/integration/js/player-local-storage-file.js
@@ -0,0 +1,103 @@
+/* global suiteSetup, suite, setup, teardown, test, assert, suiteTeardown,
+ RiseVision, sinon */
+
+/* eslint-disable func-names */
+
+var messageHandlers;
+
+suite( "file added", function() {
+  var onFileInitSpy;
+
+  suiteSetup( function() {
+    onFileInitSpy = sinon.spy( RiseVision.ImageRLS, "onFileInit" );
+
+    // mock receiving client-list message
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "client-list",
+        clients: [ "local-storage", "licensing" ]
+      } );
+    } );
+
+    // mock receiving storage-licensing message
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "storage-licensing-update",
+        isAuthorized: true,
+        userFriendlyStatus: "authorized"
+      } );
+    } );
+
+    // mock receiving file-update to notify file is downloading
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "FILE-UPDATE",
+        filePath: "risemedialibrary-30007b45-3df0-4c7b-9f7f-7d8ce6443013/widget-testing/image-widget/Gone_Girl_Book_Cover.jpg",
+        status: "STALE"
+      } );
+    } );
+
+    // mock receiving file-update to notify file is available
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "FILE-UPDATE",
+        filePath: "risemedialibrary-30007b45-3df0-4c7b-9f7f-7d8ce6443013/widget-testing/image-widget/Gone_Girl_Book_Cover.jpg",
+        status: "CURRENT",
+        ospath: "path/to/file/abc123"
+      } );
+    } );
+  } );
+
+  suiteTeardown( function() {
+    RiseVision.ImageRLS.onFileInit.restore();
+  } );
+
+  test( "should be able to set single image with correct url", function() {
+    assert( onFileInitSpy.calledOnce, "onFileInit() called once" );
+    assert( onFileInitSpy.calledWith( "file://path/to/file/abc123" ), "onFileInit() called with correct url" );
+  } );
+
+  test( "should align image at top left", function() {
+    assert.isNotNull( document.querySelector( ".top-left" ) );
+  } );
+
+  test( "should scale image to fit", function() {
+    assert.isNotNull( document.querySelector( ".scale-to-fit" ) );
+  } );
+} );
+
+suite( "file changed", function() {
+  var refreshSpy;
+
+  setup( function() {
+    refreshSpy = sinon.spy( RiseVision.ImageRLS, "onFileRefresh" );
+  } );
+
+  teardown( function() {
+    RiseVision.ImageRLS.onFileRefresh.restore();
+  } );
+
+  test( "should call onFileRefresh() and set background image", function() {
+    // mock receiving file-update to notify file is downloading
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "FILE-UPDATE",
+        filePath: "risemedialibrary-30007b45-3df0-4c7b-9f7f-7d8ce6443013/widget-testing/image-widget/Gone_Girl_Book_Cover.jpg",
+        status: "STALE"
+      } );
+    } );
+
+    // mock receiving file-update to notify file is available
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "FILE-UPDATE",
+        filePath: "risemedialibrary-30007b45-3df0-4c7b-9f7f-7d8ce6443013/widget-testing/image-widget/Gone_Girl_Book_Cover.jpg",
+        status: "CURRENT",
+        ospath: "path/to/file/def456"
+      } );
+    } );
+
+    assert( refreshSpy.calledOnce );
+    assert( refreshSpy.calledWith( "file://path/to/file/def456" ), "onFileRefresh() called with correct url" );
+  } );
+} );
---
+++ test/integration/js/player-local-storage-logging-file.js
@@ -0,0 +1,191 @@
+/* global suiteSetup, suite, setup, teardown, test, assert,
+ RiseVision, sinon */
+
+/* eslint-disable func-names */
+
+var table = "image_events",
+  params = {
+    "event": "error",
+    "event_details": "",
+    "file_url": "risemedialibrary-30007b45-3df0-4c7b-9f7f-7d8ce6443013/widget-testing/image-widget/Gone_Girl_Book_Cover.jpg",
+    "file_format": "jpg",
+    "company_id": "\"companyId\"",
+    "display_id": "\"displayId\"",
+    "version": "0.1.1"
+  },
+  ready = false,
+  messageHandlers,
+  logSpy,
+  check = function( done ) {
+    if ( ready ) {
+      sinon.stub( RiseVision.ImageRLS, "play" );
+      done();
+    } else {
+      setTimeout( function() {
+        check( done )
+      }, 1000 );
+    }
+  };
+
+suiteSetup( function( done ) {
+  check( done );
+} );
+
+teardown( function() {
+  logSpy.restore();
+} );
+
+suite( "configuration", function() {
+
+  test( "should log the configuration event", function() {
+
+    assert( logSpy.calledWith( table, {
+      "event": "configuration",
+      "event_details": "storage file",
+      "company_id": params.company_id,
+      "display_id": params.display_id,
+      "version": params.version
+    } ) );
+
+  } );
+} );
+
+suite( "errors", function() {
+  var clock;
+
+  setup( function() {
+    clock = sinon.useFakeTimers();
+  } );
+
+  teardown( function() {
+    clock.restore();
+  } );
+
+  test( "required modules unavailable", function() {
+    var i;
+
+    function receiveClientList() {
+      // mock receiving client-list message
+      messageHandlers.forEach( function( handler ) {
+        handler( {
+          topic: "client-list",
+          clients: [ "local-messaging" ]
+        } );
+      } );
+    }
+
+    logSpy = sinon.spy( RiseVision.Common.LoggerUtils, "logEvent" );
+
+    // mock 30 more client-list messages sent/received
+    for ( i = 30; i >= 0; i-- ) {
+      receiveClientList();
+      clock.tick( 1000 );
+    }
+
+    params.event_details = "required modules unavailable";
+
+    assert( logSpy.calledOnce );
+    assert( logSpy.calledWith( table, params ) );
+  } );
+
+  test( "unauthorized", function() {
+    logSpy = sinon.spy( RiseVision.Common.LoggerUtils, "logEvent" );
+
+    // mock receiving client-list message
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "client-list",
+        clients: [ "local-storage", "licensing" ]
+      } );
+    } );
+
+    // mock receiving storage-licensing message
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "storage-licensing-update",
+        isAuthorized: false,
+        userFriendlyStatus: "unauthorized"
+      } );
+    } );
+
+    params.event_details = "unauthorized";
+
+    assert( logSpy.calledOnce );
+    assert( logSpy.calledWith( table, params ) );
+
+  } );
+
+  test( "file does not exist", function() {
+    logSpy = sinon.spy( RiseVision.Common.LoggerUtils, "logEvent" );
+
+    // mock receiving client-list message
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "client-list",
+        clients: [ "local-storage", "licensing" ]
+      } );
+    } );
+
+    // mock receiving storage-licensing message
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "storage-licensing-update",
+        isAuthorized: true,
+        userFriendlyStatus: "authorized"
+      } );
+    } );
+
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "file-update",
+        filePath: params.file_url,
+        status: "NOEXIST"
+      } );
+    } );
+
+    params.event_details = "file does not exist";
+
+    assert( logSpy.calledOnce );
+    assert( logSpy.calledWith( table, params ) );
+
+  } );
+
+  test( "file error", function() {
+    logSpy = sinon.spy( RiseVision.Common.LoggerUtils, "logEvent" );
+
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "file-error",
+        filePath: params.file_url,
+        msg: "File's host server could not be reached",
+        detail: "error details"
+      } );
+    } );
+
+    params.event_details = "File's host server could not be reached";
+    params.error_details = "error details";
+
+    assert( logSpy.calledOnce );
+    assert( logSpy.calledWith( table, params ) );
+  } );
+
+  test( "file deleted", function() {
+    logSpy = sinon.spy( RiseVision.Common.LoggerUtils, "logEvent" );
+
+    messageHandlers.forEach( function( handler ) {
+      handler( {
+        topic: "file-update",
+        filePath: params.file_url,
+        status: "deleted"
+      } );
+    } );
+
+    params.event = "file deleted";
+    delete params.event_details;
+    delete params.error_details;
+
+    assert( logSpy.calledOnce );
+    assert( logSpy.calledWith( table, params ) );
+  } );
+
+} );

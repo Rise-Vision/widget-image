501dc28883dd38d7ab988c290a55b3c138151e48
Stuart Lees <stuartlees@gmail.com>
2018-07-26 15:46:05 -0400

    Fix unit tests [stage-2]

--- src/widget/image-utils.js
+++ src/widget/image-utils.js
@@ -120,7 +120,7 @@ RiseVision.ImageUtils = ( function() {
   }
 
   function logEvent( data ) {
-    data.configuration = getConfigurationType();
+    data.configuration = getConfigurationType() || "";
     RiseVision.Common.LoggerUtils.logEvent( getTableName(), data );
   }
 
